db561fb6d6a24c2b42cb7127d7993b71936613c5
Savannah Tong <tosavana@amazon.com>
2024-04-18 18:55:18 -0700

    Initial commit

---
+++ test/cdklearn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdklearn from '../lib/cdklearn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdklearn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdklearn.CdklearnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
